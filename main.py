577cde1d4555733497862429d3596c6b80afbde3
Danil <boy4rov.da@gmail.com>
2025-10-17 19:55:47 +0300

    feat: rich added

--- main.py
+++ main.py
@@ -2,18 +2,14 @@
 
 import asyncio
 
-from langchain_core._api import LangChainDeprecationWarning
 from langchain_core.runnables.schema import StreamEvent
+
+from rich.console import Console
+from rich.prompt import Prompt
 
 from src.agents import tool_agent_executor
 
-import warnings
-import colorama
-
-warnings.filterwarnings("ignore", category=LangChainDeprecationWarning)
-warnings.filterwarnings("ignore", category=UserWarning)
-
-colorama.init()
+console = Console()
 
 def pretty_print(event: StreamEvent) -> None:
     """Печатает событие из стрима
@@ -24,37 +20,39 @@ def pretty_print(event: StreamEvent) -> None:
     Returns:
         ничего не возвращает.
     """
-    cyan = colorama.Fore.CYAN
-    magenta = colorama.Fore.MAGENTA
-    reset = colorama.Fore.RESET
-
     if event["event"] == "on_chat_model_stream":
         print(event["data"]["chunk"].content, flush=True, end="")
 
     elif event["event"] == "on_tool_start":
         name = event.get("name", "")
-        msg = f"\n{magenta}=== Вызываю {name} ==={reset}"
-        print(msg)
+        msg = f"\n[magenta]=== Вызываю {name} ===[/magenta]"
+        console.print(msg)
     elif event["event"] == "on_tool_end":
         name = event.get("name", "")
-        msg = f"\n{cyan}=== Вызов {name} завершён ==={reset}"
-        print(f"\nВызов {name} завершён")
+        msg = f"\n[green]=== Вызов {name} завершён ===[/green]"
+        console.print(msg)
 
-# query = "Привет! Какие у меня мероприятия будут дальше на сегодняшний день?"
 async def main():
+    console.print(
+        "[bold green]Помощник по расписанию[/bold green]"
+    )
+    console.print(
+        "Нажмите Ctrl+C или Ctrl+D, чтобы выйти."
+    )
     try:
         while True:
-            query = input("\nВвод >>> ")
+            query = Prompt.ask("[cyan]Ввод[/cyan]")
+
             async for event in tool_agent_executor.astream_events({
                 "input": query
             }):
                 pretty_print(event)
 
     except (KeyboardInterrupt, EOFError):
-        print("\nПока!")
+        console.print("\nПока!")
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except Exception as err:
-        print("Ошибка:", err)
+        console.print(f"\n[bold red]Ошибка: {err}[/bold red]")
--- src/agents.py
+++ src/agents.py
@@ -2,15 +2,20 @@
 
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain_core._api import LangChainDeprecationWarning
 from langgraph.prebuilt import create_react_agent
 from langchain_ollama import ChatOllama
 
 from .prompts import react_prompt, default_prompt
 from .tools import tools
 from .config.config import models_params
+import warnings
+
+warnings.filterwarnings("ignore", category=LangChainDeprecationWarning)
 
 memory = ConversationBufferWindowMemory(
     memory_key="chat_history",
+    output_key="messages",
     return_messages=True,
     k=5
 )
